38056a4b2016703b99361dd8ccc6981b0cda244a
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2023-07-05 09:58:56 -0600

    Agregar archivos de proyecto.

---
+++ ViewerPDF/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using ViewerPDF.Models;
+
+namespace ViewerPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string pdfUrl = "/Home/GetPdf?pdfUrl=https://bconnectstoragetest.blob.core.windows.net/temp/dotnet_core_tutorial.pdf";
+            ViewData["pdfUrl"] = pdfUrl;
+            return View();
+        }
+
+        public async Task<IActionResult> GetPdf(string pdfUrl)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var pdfData = await httpClient.GetByteArrayAsync(pdfUrl);
+                return File(pdfData, "application/pdf");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ViewerPDF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewerPDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
